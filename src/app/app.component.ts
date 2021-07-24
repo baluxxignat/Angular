b12292f869ee780087a0049df0aa84d9a365e8b8
baluxxignat <aleksandr.pravo@gmail.com>
2021-07-24 16:39:09 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_OKTEN';
+
+
+  users = [
+    {name: 'petia', age: 31, status: true},
+    {name: 'vasia', age: 30, status: true},
+    {name: 'serhey', age: 18, status: false},
+    {name: 'olia', age: 22, status: true},
+    {name: 'yulia', age: 14, status: false},
+    {name: 'Anjey', age: 36, status: true},
+    {name: 'Simon', age: 33, status: false},
+    {name: 'Stanislav', age: 34, status: true},
+    {name: 'Kevin', age: 30, status: true},
+    {name: 'Benjamin', age: 19, status: false},
+  ];
+
+
 }
